ee1ee7890ce723ee411e882bfe801c6e048b72a9
peropero1111 <lego0707020@gmail.com>
2024-12-14 21:12:54 +0900

    index.js 업데이트

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// 실행 상태를 확인하여 조건에 따라 실행
 chrome.storage.local.get("isActive", (result) => {
-    if (!result.isActive) return; // 비활성화 상태라면 실행하지 않음
+    if (!result.isActive) return; 
 
-    // interval ID를 전역 변수에 저장하여 이후 clearInterval로 중지 가능
     window.customInterval = setInterval(() => {
         let imgs = document.querySelectorAll('img');
         imgs.forEach((a) => {
